6b91a13da18a05284114fe3409ad5ba055939075
John <jhallman5@gmail.com>
2017-10-16 16:46:48 -0700

    adds dummy data

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import Deck from './Deck'
+
+const DATA = [
+  {id: 1, text: 'Card #1', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 2, text: 'Card #2', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 3, text: 'Card #3', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 4, text: 'Card #4', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 5, text: 'Card #5', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 6, text: 'Card #6', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 7, text: 'Card #7', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+  {id: 8, text: 'Card #8', uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`},
+]
+
+export default class App extends Component<{}> {
+  renderCard(item) {
+    return (
+      <Text>{item.text}</Text>
+    )
+  }
+
+  render() {
+    return (
+      // CARD for later use
+      // <View style={styles.card}>
+      //   <Image
+      //     style={{flex: 1}}
+      //     source={{uri: `https://media-cdn.tripadvisor.com/media/photo-o/0a/56/44/5a/restaurant.jpg`}} />
+      //   <View style={{margin:20}}>
+      //     <Text style={{fontSize:20}}>Type / Price </Text>
+      //     <Text style={{fontSize: 15}}>Name / Rating </Text>
+      //   </View>
+      // </View>
+      <View style={styles.card}>
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    margin: 20,
+    marginTop: 100,
+    marginBottom: 100,
+    borderWidth: 1,
+    borderColor: 'lightgrey',
+    borderRadius: 8,
+  },
+});
---
+++ src/Deck.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import { View, Text, Animated } from 'react-native'
+
+class Deck extends Component {
+  renderCards(){
+    return this.props.data.map(item => {
+      return this.props.renderCard(item)
+    })
+  }
+  render() {
+    console.log(this.props)
+    return (
+      <View>
+        {this.renderCards()}
+      </View>
+    )
+  }
+}
+
+export default Deck
